54892c0f281aa6a4742680eedaf5de75f7814d04
zxzxk0 <zxzxk0@naver.com>
2022-01-04 01:11:25 +0900

    프로그래머스 2016년

---
+++ programmers-lv1/year 2016.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers) {
+    int sum=0;
+    for(int i=0;i<numbers.size();i++){
+        sum+=numbers[i];
+    }
+    return 45-sum;
+}
