4ca26ffa37b7c64a940af4d7c209d925c5e95cec
zxzxk0 <zxzxk0@naver.com>
2022-01-08 00:56:58 +0900

    프로그래머스 부족한 금액 계산하기

---
+++ programmers-lv1/calculating the left over.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long answer=0;
+    for(int i=1;i<=count;i++){
+        answer+=price*i;
+    }
+    if(answer<money){return 0;}
+    return answer-money;
+}
