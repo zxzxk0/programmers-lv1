06fe3065e58a8def15fcd2f0bad98e2ee09047d7
zxzxk0 <zxzxk0@naver.com>
2022-01-09 11:58:02 +0900

    프로그래머스 모의고사

--- programmers-lv1/mock exam.cpp
+++ programmers-lv1/mock exam.cpp
@@ -1,12 +1,27 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int solution(vector<int> numbers) {
-    int sum=0;
-    for(int i=0;i<numbers.size();i++){
-        sum+=numbers[i];
+
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    int test1[5] = {1, 2, 3, 4, 5};
+    int test2[8] = {2, 1, 2, 3, 2, 4, 2, 5};
+    int test3[10] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    int score[3] = {0, 0, 0};
+    int max_score = 0;
+    for (int i = 0; i < answers.size(); i++){
+        if (answers[i] == test1[i % 5]) score[0] += 1;
+        if (answers[i] == test2[i % 8]) score[1] += 1;
+        if (answers[i] == test3[i % 10]) score[2] += 1;
     }
-    return 45-sum;
+    max_score = max(max(score[0], score[1]), score[2]);
+    for (int i = 0; i < 3; i++){
+        if (score[i] == max_score)
+            answer.push_back(i + 1);
+    }
+    return answer;
 }
