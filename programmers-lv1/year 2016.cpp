bf0ece105b9f943ef48de278b351e251e70f6132
zxzxk0 <zxzxk0@naver.com>
2022-01-09 11:58:46 +0900

    프로그래머스 2016년

--- programmers-lv1/year 2016.cpp
+++ programmers-lv1/year 2016.cpp
@@ -2,11 +2,15 @@
 #include <vector>
 
 using namespace std;
-
-int solution(vector<int> numbers) {
-    int sum=0;
-    for(int i=0;i<numbers.size();i++){
-        sum+=numbers[i];
+string day[7]={"THU","FRI","SAT","SUN","MON","TUE","WED"};
+int year[12]={31,29,31,30,31,30,31,31,30,31,30,31};
+string solution(int a, int b) {
+    string answer = "";
+    int temp=0;
+    for(int i=0;i<a-1;i++){
+        temp+=year[i];
     }
-    return 45-sum;
+    temp+=b;
+    answer=day[temp%7];
+    return answer;
 }
