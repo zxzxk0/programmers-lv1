ece311fc4a037d5805463e6a17cf9aa6f250fc8b
zxzxk0 <zxzxk0@naver.com>
2022-01-09 11:58:31 +0900

    프로그래머스 두 개 뽑아서 더하기

--- programmers-lv1/pick two and add it.cpp
+++ programmers-lv1/pick two and add it.cpp
@@ -1,12 +1,18 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
-int solution(vector<int> numbers) {
+vector<int> solution(vector<int> numbers) {
+    vector<int> answer;
     int sum=0;
-    for(int i=0;i<numbers.size();i++){
-        sum+=numbers[i];
+    for(int i=0;i<numbers.size()-1;i++){
+        for(int j=i+1;j<numbers.size();j++){
+            sum=numbers[i]+numbers[j];
+            answer.push_back(sum);
+        }
     }
-    return 45-sum;
+    sort(answer.begin(),answer.end());
+    answer.erase(unique(answer.begin(),answer.end()),answer.end());
+    return answer;
 }
