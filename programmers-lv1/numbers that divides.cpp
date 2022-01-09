3aaf07876df9f3613b2a1cf9b4c05d784e6f587f
zxzxk0 <zxzxk0@naver.com>
2022-01-09 11:59:08 +0900

    프로그래머스 나누어 떨어지는 숫자 배열

--- programmers-lv1/numbers that divides.cpp
+++ programmers-lv1/numbers that divides.cpp
@@ -1,12 +1,18 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
-int solution(vector<int> numbers) {
-    int sum=0;
-    for(int i=0;i<numbers.size();i++){
-        sum+=numbers[i];
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]%divisor==0){
+            answer.push_back(arr[i]);
+        }
     }
-    return 45-sum;
+    if(answer.size()==0){
+        answer.push_back(-1);
+    }
+    sort(answer.begin(),answer.end());
+    return answer;
 }
